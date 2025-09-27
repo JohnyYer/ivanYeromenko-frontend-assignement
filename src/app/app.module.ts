199fc3da06fa3eabf932d8df216bddb797b99dae
Ivan Yeromenko <johnyeremenko91@gmail.com>
2025-09-27 21:43:16 +0200

    Material design. Header redesigned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,11 @@ import { ClientFormComponent } from '@features/clients/client-form/client-form.c
 import { FormsModule } from '@angular/forms';
 import { DateFormatPipe } from '@shared/pipes/date-format.pipe';
 
+// Angular Material imports
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +28,16 @@ import { DateFormatPipe } from '@shared/pipes/date-format.pipe';
     ClientFormComponent,
     DateFormatPipe,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
